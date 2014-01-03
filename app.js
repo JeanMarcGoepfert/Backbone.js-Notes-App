15c0e6cf2e79c07f9027691f65b1ee909063f6bb
Jean-Marc Goepfert <jeanmarcgoepfert@Jean-Marcs-MacBook-Pro.local>
2014-01-03 11:06:57 +1100

    initial set up

---
+++ app.js
@@ -0,0 +1,5 @@
+var sampleData = [
+    { title: "Learn Backbone", details: "learn backbone they said, it'll be fun they said" },
+    { title: "Build an app", details: "i dunno what as long as it's awesome...facebook for dogs maybe?" },
+    { title: "Profit", details: "Money in the bank!" }
+];
